8d5a63df07669118de713823c949ab2f2584f3e4
zaboza6 <161701048+zaboza6@users.noreply.github.com>
2024-02-29 11:05:27 +0100

    Create main.c
    init project

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int ft_strlen(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+int main()
+{
+	char str[] = "Hello";
+	printf("%d\n", ft_strlen(str));
+	return 0;
+}
